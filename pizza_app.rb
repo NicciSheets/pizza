aa59f64dfdf52a80486343876d96f229c7df0241
Nicci Sheets <niccisheets@aol.com>
2018-06-18 16:02:31 -0400

    allowing choices for toppings

--- pizza_app.rb
+++ pizza_app.rb
@@ -2,17 +2,8 @@ puts "How many slices would you like?"
 
 pies = gets.chomp.to_i
 
-puts "How many toppings would you like?"
-
-toppings = gets.chomp.to_i
-
 slice_price = 2.00 * pies 
-total_topping_price = 0.50 * toppings
 
-
-def meat_toppings
-	meat = ["pepperoni", "sausage", "ham", "chicken", "bacon"].sample
-end
 
 def sauce_type
 	sauce = ["marinara", "alfredo", "barbeque"].sample
@@ -26,14 +17,37 @@ def pizza_size
 	size = ["small", "medium", "large"].sample
 end
 
-def total_price(total_topping_price,slice_price)
-	puts "total_topping_price#{total_topping_price}"
-	puts "slice_price #{slice_price}"
-	total_topping_price + slice_price
+def meat_toppings
+	meat = ["pepperoni", "sausage", "ham", "chicken", "bacon", "none"]
+end
+
+
+puts "What meat toppings would you like to add?"
+
+meat = gets.chomp
+
+def meat
+	if pepperoni
+		puts slice_price += 1.00
+	elsif sausage
+		puts slice_price += 1.00
+	elsif ham
+		puts slice_price += 0.50
+	elsif chicken
+		puts slice_price += 1.50
+	elsif bacon 
+		puts slice_price += 1.00
+	else none
+		puts slice_price 
+	end
 end
 
 
 
-#total(pies, price)
+def total_price(meat)
+	puts meat
+end
 
-puts "This is $#{total_price(total_topping_price,slice_price)}. This is number of pies #{pies}, with #{meat_toppings}, #{sauce_type} and #{veg_options}.  All size #{pizza_size}"
+
+
+puts "This is #{total_price(meat)}. This is number of pies #{pies}, with #{meat}, #{sauce_type} and #{veg_options}.  All size #{pizza_size}"
