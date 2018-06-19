54685d164b46cfbf52eace1861c0f538f09b0870
Nicci Sheets <niccisheets@aol.com>
2018-06-19 15:35:52 -0400

    working on adding prices

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,53 +1,42 @@
-puts "How many slices would you like?"
+class Pizza
+	attr_accessor :quantity, :crust, :toppings
 
-pies = gets.chomp.to_i
-
-slice_price = 2.00 * pies 
-
-
-def sauce_type
-	sauce = ["marinara", "alfredo", "barbeque"].sample
-end
-
-def veg_options
-	vegs = ["green peppers", "mushrooms", "banana peppers", "onions"].sample
-end
-
-def pizza_size
-	size = ["small", "medium", "large"].sample
-end
-
-def meat_toppings
-	meat = ["pepperoni", "sausage", "ham", "chicken", "bacon", "none"]
-end
-
-
-puts "What meat toppings would you like to add?"
-
-meat = gets.chomp
-
-def meat
-	if pepperoni
-		puts slice_price += 1.00
-	elsif sausage
-		puts slice_price += 1.00
-	elsif ham
-		puts slice_price += 0.50
-	elsif chicken
-		puts slice_price += 1.50
-	elsif bacon 
-		puts slice_price += 1.00
-	else none
-		puts slice_price 
+	def pizza_order
+		"That'll be #{quantity} slices of #{crust} pizza, with #{toppings}."
 	end
 end
 
+pizza = Pizza.new
 
+puts "Welcome to Nicci's Pizza Barn.  How many slices would you like?"
+pizza.quantity = gets.chomp.to_i
 
-def total_price(meat)
-	puts meat
+puts "Would you like pan crust or hand tossed?"
+pizza.crust = gets.chomp
+
+puts "And what toppings would you like? We offer pepperoni, sausage, ham, bacon, green peppers, banana peppers, olives, mushrooms, or onions.  You can also add extra cheese."
+pizza.toppings = gets.chomp
+
+puts pizza.pizza_order
+
+def crust_price
+	crust = ["pan crust", "hand tossed"]
+	if crust == "pan crust"
+		"#{quantity}*3.00"
+	else crust == "hand tossed"
+		"#{quantity}*2.00"
+	end
+end
+
+def total_price(crust_price)
+	puts "crust_price#{crust_price}"
+	crust_price
 end
 
 
 
-puts "This is #{total_price(meat)}. This is number of pies #{pies}, with #{meat}, #{sauce_type} and #{veg_options}.  All size #{pizza_size}"
+puts "Your total will be #{total_price(crust_price)}."
+
+
+
+
