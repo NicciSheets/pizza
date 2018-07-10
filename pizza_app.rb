439fc1e53a66aafc7f6051bbdd5b3395373c498f
Nicci Sheets <niccisheets@aol.com>
2018-07-09 22:16:52 -0400

    cleaned up code

--- pizza_app.rb
+++ pizza_app.rb
@@ -17,7 +17,6 @@ puts pizza_order(pizza_quantity, pizza_crust, pizza_toppings)
 def crust_subtotal(pizza_crust)
 	"pizza_crust = #{pizza_crust}"
 	crust_subtotal_arr = []
-	#pizza_crust = ["pan crust", "hand tossed"]
 	crust_subtotal = 0
 	if pizza_crust == "pan crust"
 		return crust_subtotal = 3.00
@@ -28,66 +27,13 @@ def crust_subtotal(pizza_crust)
 	crust_subtotal_arr.to_s 
 	crust_subtotal_arr.join
 end
-#puts crust_subtotal(pizza_crust)
-#crust_subtotal(pizza_crust)
+
 
 
 def pizza_toppings_split(pizza_toppings) ####I need to do a regex here to take out any punctuation such as commas that user may input************************
 	pizza_toppings.split
 end
 
-#def pizza_toppings_nopunct(pizza_toppings)
-#p pizza_toppings_split(pizza_toppings)
-#end
-
-#puts pizza_toppings_nopunct(pizza_toppings)
-#p pizza_toppings_split(pizza_toppings)
-
-# def toppings_choices(pizza_toppings) 
-# 	toppings_subtotal_arr = []
-# 	toppings_subtotal = 0
-# 	#choices = ["pepperoni", "sausage", "ham", "bacon", "green peppers", "banana peppers", "olives", "mushrooms", "onions", "pineapple" ,"extra cheese"]
-# 	pizza_toppings_split(pizza_toppings) do |topping|
-# 		if topping == "pepperoni" || "sausage"
-# 			return toppings_subtotal += 0.50
-# 		elsif topping == "ham" || "bacon"
-# 			return toppings_subtotal += 0.75
-# 		elsif topping == "green peppers" || "banana peppers" || "olives" || "mushrooms" || "onions"
-# 			return toppings_subtotal += 0.25
-# 		else topping == "pineapple" || "extra cheese"
-# 			return toppings_subtotal += 1.00
-# 		end
-# 		toppings_subtotal.to_i
-# 	end
-# end
-
-
-
-# def toppings_subtotal(pizza_toppings, toppings_choices)
-# 	"pizza_toppings = #{pizza_toppings}"
-# 	"toppings_choices = #{toppings_choices}"
-# 	toppings_subtotal_arr = []
-# 	pizza_toppings.each do |price|
-# 		if pizza_toppings == toppings_choices.index
-# 				return toppings_choices[price]
-# 		end
-# 	end
-
-	# toppings_subtotal = 0
-	# if choices == "pepperoni" || "sausage"
-	# 	return toppings_subtotal += 0.50
-	# elsif choices == "ham" || "bacon"  
-	# 	return toppings_subtotal += 0.75
-	# elsif choices == "pineapple" || "extra cheese"
-	# 	return toppings_subtotal += 1.00
-	# else choices == "green peppers" || "banana peppers" || "olives" || "mushrooms" || "onions"
-	# 	return toppings_subtotal += 0.25
-	# 	toppings_subtotal
-	# end
-	# toppings_subtotal_arr << toppings_subtotal
-	# toppings_subtotal_arr.to_i
-
-#puts toppings_subtotal(pizza_toppings)
 
 def toppings(pizza_toppings)
 	empty = []
@@ -108,8 +54,6 @@ def subtotal_array(pizza_toppings, pizza_crust)
 	subtotal_arr = [[toppings(pizza_toppings)], [crust_subtotal(pizza_crust)]] 
 	subtotal_arr.flatten.sum
 end
-
-#p subtotal_array(pizza_toppings, pizza_crust)
 
 
 def total(pizza_toppings, pizza_crust, pizza_quantity)
